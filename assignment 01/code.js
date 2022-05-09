9335fec6f5e609d583a467fb0b6894dae38088d6
GhilliedFox <richardcfenton@gmail.com>
2022-05-08 23:09:04 -0700

    added filtering and css
    added filtering and css

--- assignment 01/code.js
+++ assignment 01/code.js
@@ -2,6 +2,7 @@ const poke_container = document.getElementById("poke_container");
 const searchButton = document.getElementById("search-btn");
 const pokemonClicked = document.getElementById("pokemonClicked");
 const result = document.getElementById("#result");
+var pokemon = {};
 
 const colors = {
   fire: "#FDDFDF",
@@ -63,6 +64,88 @@ const getPokemon = async (query) => {
   createPokemonCard(pokemon);
 };
 
+const getPokemonByType = async (query) => {
+  const url = `https://pokeapi.co/api/v2/type/${query}`;
+  const response = await fetch(url);
+  const pokemon = await response.json();
+  console.log(pokemon);
+  createPokemonCardByType(pokemon);
+};
+
+function createPokemonCardByType(pokemon) {
+  for (let i = 0; i < pokemon.pokemon.length; i++) {
+    const poke_card = document.createElement("div");
+    poke_card.classList.add("pokemon");
+    const poke_cardHTML = `
+    <a onclick='' id="pokemonClicked">
+    <div class="poke-card-info">
+    <h3>${pokemon.pokemon[i].pokemon.name}</h3></a>
+    </div>
+    `;
+    // if the a tag is clicked, the pokemon profile will be displayed
+    poke_card.addEventListener("click", function () {
+      getPokemonOnClick(pokemon.id);
+    });
+    poke_card.innerHTML = poke_cardHTML;
+    poke_container.appendChild(poke_card);
+  }
+}
+
+const getPokemonOnClick = async (query) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${query}`;
+  const response = await fetch(url);
+  const pokemon = await response.json();
+  console.log(pokemon);
+  createPokemonProfile(pokemon);
+};
+
+function createPokemonProfile(pokemon) {
+  poke_container.innerHTML = "";
+  document
+    .getElementById("poke_container")
+    .setAttribute("class", "poke_profile");
+  const poke_card = document.createElement("div");
+  poke_card.classList.add("pokemon");
+  const pokeType = `${pokemon.types[0].type.name}`;
+  const color = colors[pokeType];
+  const hp_max = (pokemon.stats[0].base_stat / 255) * 100;
+  const attk_max = (pokemon.stats[1].base_stat / 150) * 100;
+  const def_max = (pokemon.stats[2].base_stat / 150) * 100;
+  const speed_max = (pokemon.stats[3].base_stat / 150) * 100;
+  poke_card.style.backgroundColor = color;
+  const poke_cardHTML = `
+    <div class="poke-profile-img">
+    <h1>${pokemon.name}</h1>
+    <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+  </div>
+  <div class="poke-stat-container">
+  <div class="poke-profile-info">
+    <h2>ID: ${pokemon.id}</h2>
+    <h2>Type: ${pokemon.types[0].type.name}</h2>
+    <h2>Height: ${pokemon.height}</h2>
+    <h2>Weight: ${pokemon.weight}</h2>
+    <h2>Abilities: ${pokemon.abilities[0].ability.name}</h2>
+  </div>
+  <div class="poke-profile-stats">
+    <h2>HP: ${pokemon.stats[0].base_stat}</h2>  <div class="meter">
+    <span style="width: ${hp_max}%"></span>
+  </div>
+    <h2>Attack: ${pokemon.stats[1].base_stat}</h2><div class="meter red">
+    <span style="width: ${attk_max}%"></span>
+  </div>
+    <h2>Defense: ${pokemon.stats[2].base_stat}</h2><div class="meter orange">
+    <span style="width: ${def_max}%"></span>
+  </div>
+    <h2>Speed: ${pokemon.stats[5].base_stat}</h2><div class="meter orange">
+    <span style="width: ${speed_max}%"></span>
+  </div>
+    </div>
+    </div>
+      `;
+  poke_card.innerHTML = poke_cardHTML;
+  poke_container.appendChild(poke_card);
+}
+
 function createPokemonCard(pokemon) {
   const poke_card = document.createElement("div");
   poke_card.classList.add("pokemon");
@@ -74,19 +157,21 @@ function createPokemonCard(pokemon) {
   <div class="poke-card-img">
   <h2>
 #${pokemon.id}</h2>
-<a href="/profile/${pokemon.name}" target="_blank" id="pokemonClicked">
+<a onclick='' id="pokemonClicked">
   <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}"></a>
 </div>
 <div class="poke-card-info">
-  <h2>${pokemon.name}</h3>
+  <h2>${pokemon.name}</h2>
   <h3>type: ${pokemon.types[0].type.name}</h3>
 </div>
     `;
+  // if the a tag is clicked, the pokemon profile will be displayed
+  poke_card.addEventListener("click", function () {
+    getPokemonOnClick(pokemon.id);
+  });
   poke_card.innerHTML = poke_cardHTML;
   poke_container.appendChild(poke_card);
 }
-
-function createPokemonProfile(pokemon) {}
 
 function searchPokemon() {
   document
@@ -102,7 +187,9 @@ function searchPokemon() {
 function buttonPressed() {
   //clear the container
   poke_container.innerHTML = "";
-  // if the
+  document
+    .getElementById("poke_container")
+    .setAttribute("class", "poke_container");
   // if the search bar is not empty then i = contents of the search bar
   if (document.getElementById("search").value !== "") {
     const i = document.getElementById("search").value;
@@ -113,9 +200,11 @@ function buttonPressed() {
     if (document.getElementById("poke_type").value === "all") {
       fetchPokemonByRegion();
     } else {
-      // if the type is not all then run the fetchPokemonByTypeandRegion function
+      // if the type is not all then run the fetchPokemonByType function
       if (document.getElementById("poke_type").value != "all") {
-        fetchPokemonByTypeandRegion();
+        const i = document.getElementById("poke_type").value;
+        console.log(i);
+        getPokemonByType(i);
       }
     }
   }
